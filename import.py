acd47b90dd4feb9e3c46cad0b67bee454b740f7d
dhanukaperera <dhanukashehanperera@gmail.com>
2023-02-22 18:35:56 +0000

    import all data in data folder

--- import.py
+++ import.py
@@ -1,20 +1,35 @@
 import csv
+import glob
 import datetime
 from app import app, db, models
 
+csv_directory = './data'
+
+csv_files = glob.glob(csv_directory + '/*.csv')
+
+headers = ['business_date', 'country_name', 'currency_code', 'exchange_rate']
+
 def import_csv():
-    with open('/Users/dhanukaperera/Projects/QuantSpark/data/fx_rates_1720230124.csv',mode='r', encoding='utf-8-sig') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            try:
-                business_date = datetime.datetime.strptime(row['business_date'], '%d/%m/%Y').date()
-                exchange_rate = float(row['exchange_rate'])
-                fxrate = models.FXRate(business_date=business_date, country_name=row['country_name'], 
-                                currency_code=row['currency_code'], exchange_rate=exchange_rate)
-                db.session.add(fxrate)
-            except (ValueError, TypeError):
-                app.logger.warning(f"Invalid data: {row}")
-    db.session.commit()
+    for csv_file in csv_files:
+        with open(csv_file,mode='r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            header_row = next(reader)
+
+            if all(header in header_row for header in headers):
+                print('All headers exist in the CSV file')
+                for row in reader:
+                    try:
+                        business_date = datetime.datetime.strptime(row['business_date'], '%d/%m/%Y').date()
+                        exchange_rate = float(row['exchange_rate'])
+                        fxrate = models.FXRate(business_date=business_date, country_name=row['country_name'], 
+                                    currency_code=row['currency_code'], exchange_rate=exchange_rate)
+                        db.session.add(fxrate)
+                    except (ValueError, TypeError):
+                        app.logger.warning(f"Invalid data: {row}")
+                db.session.commit()
+            else:
+                missing_headers = [header for header in headers if header not in header_row]
+                print('The following headers are missing in the CSV file: {}'.format(missing_headers))
 
 with app.app_context():
     import_csv()
